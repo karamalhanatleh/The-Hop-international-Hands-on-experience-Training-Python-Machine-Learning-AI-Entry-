cd48bf1266c76a4b453ae9f720f712b6b53c4dab
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-02-21 10:58:11 +0300

    Add files via upload

---
+++ python/Simple/main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def foo():
+    """ function foo() comunents help to you  """
+
+    pass
+print(foo.__doc__)
+
